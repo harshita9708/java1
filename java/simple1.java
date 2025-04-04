10ed2eafd55923dfa389f24dc2f58cbeede9fb56
harshita9708 <harshitahosamani04@gmail.com>
2025-04-04 09:22:27 +0530

    save the master branch

---
+++ java/simple1.java
@@ -0,0 +1,14 @@
+public class simple1 {
+    
+    public static void main(String[] args)
+        {
+            int a= 10;
+            int b= 20;
+            System.out.println("addition: "+(a+b));
+            System.out.println("Subtraction: "+(a-b));
+            System.out.println("Multiplication: "+(a*b));
+            System.out.println("Division: "+(a/b));
+            System.out.println("Modulo:"+(a%b));
+            
+        }
+}
