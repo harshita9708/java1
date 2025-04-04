500ffee07bf8703dca94c7047306f9071380f704
harshita9708 <harshitahosamani04@gmail.com>
2025-04-04 08:52:22 +0530

    save

---
+++ java/simple.java
@@ -0,0 +1,13 @@
+public class simple {
+        public static void main(String[] args)
+        {
+            int a= 10;
+            int b= 20;
+            System.out.println("addition: "+(a+b));
+            System.out.println("Subtraction: "+(a-b));
+            System.out.println("Multiplication: "+(a*b));
+            System.out.println("Division: "+(a/b));
+            
+        }
+    }  
+}
